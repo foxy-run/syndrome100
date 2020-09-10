7141966148147ce54eac919903341fb06971d5e5
Xenia Bocharova <bochxenia@yandex.ru>
2020-09-10 20:19:17 +0300

    added test

---
+++ src/test/java/ru/netology/statictic/StatisticsServiceTest.java
@@ -0,0 +1,12 @@
+package ru.netology.statictic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+    }
+}
